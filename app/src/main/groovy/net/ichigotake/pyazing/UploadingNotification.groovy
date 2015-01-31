a3c1003105c6d229d3405d08a53dc4565fbed244
ichigotake <ichigotake.san@gmail.com>
2015-02-01 00:34:13 +0900

    Cache to NotificationManager

--- app/src/main/groovy/net/ichigotake/pyazing/UploadingNotification.groovy
+++ app/src/main/groovy/net/ichigotake/pyazing/UploadingNotification.groovy
@@ -14,9 +14,11 @@ final class UploadingNotification {
     private final String NOTIFICATION_TAG_UPLOAD_COMPLETE = "upload_complete";
     private final String NOTIFICATION_PROGRESS = "progress";
     private final Context context;
+    private final NotificationManager notificationManager;
 
     UploadingNotification(Context context) {
         this.context = context;
+        this.notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     }
 
     void startProgress() {
@@ -27,12 +29,10 @@ final class UploadingNotification {
                 .setProgress(100, 0, true)
                 .setOngoing(true)
                 .build();
-        NotificationManager notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
         notificationManager.notify(NOTIFICATION_PROGRESS, R.string.app_name, notification);
     }
 
     void stopProgress() {
-        NotificationManager notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
         notificationManager.cancel(NOTIFICATION_PROGRESS, R.string.app_name);
     }
 
@@ -60,7 +60,6 @@ final class UploadingNotification {
         builder.addAction(R.drawable.ic_action_share,
                 context.getString(R.string.app_share),
                 createShareIntent(url));
-        NotificationManager notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
         notificationManager.notify(NOTIFICATION_TAG_UPLOAD_COMPLETE, R.string.app_name, builder.build());
         Toasts.completeUploading(context);
     }
@@ -90,7 +89,6 @@ final class UploadingNotification {
                 .addAction(R.drawable.ic_launcher,
                         context.getString(R.string.app_upload_retry), createRetryUploadIntent(media))
                 .build();
-        NotificationManager notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
         notificationManager.notify(R.string.app_name, notification);
     }
 
